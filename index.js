95d20f4c30fc31fbd8532a37ac3c35ff00de7e14
Parker Williamson <parker.williamson@gmail.com>
2020-08-05 17:28:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 
 document.querySelector("main#main").remove(); 
 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Parker is the champion!";
+newHeader.className = "victory";
